654616c26eb86a0f5ca9a5b1a426c8f83db31bcd
kyjita <kyjita@gmail.com>
2024-06-07 21:43:41 +0800

    增加relation

--- models/order.go
+++ models/order.go
@@ -6,8 +6,9 @@ type Order struct {
 	gorm.Model
 	UserID        uint
 	OrderItemID   uint
-	Status        string  `gorm:"default:'pending'"`
-	Price         float64 `gorm:"default:0"`
-	PaymentMethod string  `gorm:"default:''"`
-	PaymentStatus string  `gorm:"default:''"`
+	Status        string    `gorm:"default:'pending'"`
+	Price         float64   `gorm:"default:0"`
+	PaymentMethod string    `gorm:"default:''"`
+	PaymentStatus string    `gorm:"default:''"`
+	OrderItem     OrderItem `gorm:"foreignKey:OrderItemID"`
 }
